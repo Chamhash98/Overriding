6702fd610bf304d3397d72359c6bf0423a5f2b59
Chamhash98 <hashiath869@gmail.com>
2023-11-06 00:43:45 +0530

    initial commit

---
+++ src/Tree.java
@@ -0,0 +1,15 @@
+public class Tree {
+    void gain(){
+        System.out.println("Growing!");
+    }
+}
+class Mango extends Tree{
+    void gain(){
+        System.out.println("Growing slow!");
+    }
+
+    public static void main(String[] args) {
+        Mango m = new Mango();
+        m.gain();
+    }
+}
